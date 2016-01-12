153aa4691411192f43c325a4ea457d346e9b21b2
Oliver Low at MasonBullock <oliver.low@masonbullock.onmicrosoft.com>
2016-01-12 16:46:24 +0000

    Moved to serving UI1 from node.js
    Created casenode.js the main node.js application, requiring Express
    Created two routers:
    /user - for handling user URLs
    /db - for handlign DB interaction
    They don't do much yet, but the structure is there.
    NB - need to look again at the use of static and the various paths we use

---
+++ dbRouter.js
@@ -0,0 +1,37 @@
+/*
+dbRouter.js
+CaseNode user node.js express router for route /db
+This is the main handler for database interactions
+VERSION: 0.1
+Revision history:
+v 0.1 - 12 Jan 2016 incept
+*/
+var express=require("express"); //Express app base
+var bodyParser=require("body-parser"); //middleware for parsing POST requests
+var router=express.Router(); //this router (/db)
+
+//middleware to log date
+router.use(function(req, res, next){
+	//log the request with the date (from contructor function)
+	console.log("Router /user " + req.originalUrl + " " + new Date().toLocaleDateString());
+	next();
+});
+
+//middleware to parse POST requests
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// default GET route = bad request
+router.get("/", function(req, res) {
+	res.status(400).send("This is the database service - you shouldn't be here!");
+});
+
+//
+router.post("/", function(req, res) {
+	//where is this coming from? 
+	res.write(JSON.stringify(req.body));
+	res.end();
+});
+
+//export the module for the main app to use
+module.exports=router;
